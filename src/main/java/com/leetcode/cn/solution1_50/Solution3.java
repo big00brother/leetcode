1b3c959db2167b34f1929ab234a41fbaca529a40
hao.lv <1047627965@qq.com>
2024-01-12 10:34:12 +0800

    第三题题解

--- src/main/java/com/leetcode/cn/solution1_50/Solution3.java
+++ src/main/java/com/leetcode/cn/solution1_50/Solution3.java
@@ -1,9 +1,29 @@
 package com.leetcode.cn.solution1_50;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution3 {
+
+    public int lengthOfLongestSubstring1(String s) {
+        int max = 0;
+        for(int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+            int j = i;
+            for(; j < s.length(); j++) {
+                char a = s.charAt(j);
+                if(!set.contains(a)) {
+                    set.add(a);
+                } else {
+                    break;
+                }
+            }
+            max = Math.max(max, j - i);
+        }
+        return max;
+    }
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Integer> map = new HashMap<>();
         int max = 0;
