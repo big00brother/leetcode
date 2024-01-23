b3a947df6525bc8024fe674093960ae85c160490
hao.lv <1047627965@qq.com>
2024-01-23 17:36:22 +0800

    15

--- src/main/java/com/leetcode/cn/solution1_50/Solution15.java
+++ src/main/java/com/leetcode/cn/solution1_50/Solution15.java
@@ -8,19 +8,23 @@ public class Solution15 {
         List<List<Integer>> list = new ArrayList<>();
         // 前两个数字相加后，便于查找相加为0第三个数字是否存在
         Map<Integer, Integer> map = new HashMap<>();
-        // 判断是否有重复元组
-        Set<String> set = new HashSet<>();
 
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
 
         }
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i] > 0) {
                 break;
             }
-            for (int j = i + 1; j < nums.length; j++) {
+            if (i != 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
                 if (nums[i] + nums[j] > 0) {
                     break;
                 }
@@ -30,14 +34,10 @@ public class Solution15 {
                     bd.append(nums[i]);
                     bd.append(nums[j]);
                     String s = bd.toString();
-                    if (!set.contains(s)) {
-                        list1.add(nums[i]);
-                        list1.add(nums[j]);
-                        list1.add(-nums[i] - nums[j]);
-                        list.add(list1);
-                        set.add(s);
-
-                    }
+                    list1.add(nums[i]);
+                    list1.add(nums[j]);
+                    list1.add(-nums[i] - nums[j]);
+                    list.add(list1);
                 }
 
             }
