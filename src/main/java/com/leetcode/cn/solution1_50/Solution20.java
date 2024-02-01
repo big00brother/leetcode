1cdfc1573d3bdf2f0f8ae6ac96fe055997934c8d
hao.lv <1047627965@qq.com>
2024-02-01 11:37:56 +0800

    20

--- src/main/java/com/leetcode/cn/solution1_50/Solution20.java
+++ src/main/java/com/leetcode/cn/solution1_50/Solution20.java
@@ -5,22 +5,22 @@ import java.util.Stack;
 public class Solution20 {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(c == '(' || c == '{' || c == '[') {
+            if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            } else if (c == ')'){
-                if(stack.isEmpty() || stack.peek() != '(') {
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
                     return false;
                 }
                 stack.pop();
-            } else if (c == '}'){
-                if(stack.isEmpty() || stack.peek() != '{') {
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.peek() != '{') {
                     return false;
                 }
                 stack.pop();
-            } else if (c == ']'){
-                if(stack.isEmpty() || stack.peek() != '[') {
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
                     return false;
                 }
                 stack.pop();
