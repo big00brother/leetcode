5b15bb9c67a1d8f2bcdd22fe5f66ffb05af2496d
hao.lv <1047627965@qq.com>
2024-02-01 11:06:54 +0800

    20

---
+++ src/main/java/com/leetcode/cn/solution1_50/Solution20.java
@@ -0,0 +1,33 @@
+package com.leetcode.cn.solution1_50;
+
+import java.util.Stack;
+
+public class Solution20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')'){
+                if(stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            } else if (c == '}'){
+                if(stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            } else if (c == ']'){
+                if(stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
