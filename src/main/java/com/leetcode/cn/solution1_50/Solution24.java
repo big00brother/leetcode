36fc8085bc8b4f48be338b5ce6dfae74142db522
hao.lv <1047627965@qq.com>
2024-02-04 17:39:55 +0800

    24

--- src/main/java/com/leetcode/cn/solution1_50/Solution24.java
+++ src/main/java/com/leetcode/cn/solution1_50/Solution24.java
@@ -48,12 +48,17 @@ public class Solution24 {
         for (int i = 0; i < list1.size(); i++) {
             ListNode node = list1.get(i);
             node.next = list2.get(i);
+            //list1没遍历到尾部
             if (i != list1.size() - 1) {
+                //list1后面还有结点
                 node.next.next = list1.get(i + 1);
             } else {
+                //list2比list1多一个元素
                 if (list2.size() > list1.size()) {
+                    //list2的最后一个元素是最后一个结点
                     node.next.next = list2.get(i + 1);
                 } else {
+                    //list1和list2元素一样多，list2最后一个结点的next指针指向null
                     node.next.next = null;
                 }
             }
