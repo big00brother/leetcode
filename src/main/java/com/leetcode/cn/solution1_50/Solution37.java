173ecbc312b116ba9a4e86fb585c301e03e6f5b1
hao.lv <1047627965@qq.com>
2024-02-28 14:46:40 +0800

    37

--- src/main/java/com/leetcode/cn/solution1_50/Solution37.java
+++ src/main/java/com/leetcode/cn/solution1_50/Solution37.java
@@ -10,6 +10,7 @@ public class Solution37 {
         if (i == 9) {
             return true;
         }
+        // nextI和nextJ是下一个遍历的字符位置
         int nextI = i;
         int nextJ = j;
         if (j == 8) {
@@ -27,6 +28,7 @@ public class Solution37 {
                 if (dfs(board, nextI, nextJ)) {
                     return true;
                 } else {
+                    // 以当前输入的数字进行后续dfs时，后续dfs返回false，则当前字符需要重新置为'.'
                     board[i][j] = '.';
                 }
             }
@@ -34,16 +36,17 @@ public class Solution37 {
         return false;
     }
 
+    // 检查当前数字是否符合数独规则
     public boolean isValidSudoku(char[][] board, int i, int j, char c) {
-        for (int row = 0; row < 9; row++) {//行是否合法
+        for (int row = 0; row < 9; row++) {// 行是否合法
             if (board[row][j] == c)
                 return false;
         }
-        for (int col = 0; col < 9; col++) {//列是否合法
+        for (int col = 0; col < 9; col++) {// 列是否合法
             if (board[i][col] == c)
                 return false;
         }
-        for (int row = i / 3 * 3; row < i / 3 * 3 + 3; row++) {//小的block是否合法
+        for (int row = i / 3 * 3; row < i / 3 * 3 + 3; row++) {// 小的3*3格子是否合法
             for (int col = j / 3 * 3; col < j / 3 * 3 + 3; col++) {
                 if (board[row][col] == c)
                     return false;
