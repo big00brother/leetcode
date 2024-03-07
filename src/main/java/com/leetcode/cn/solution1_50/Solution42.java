4569bd68940cb1f1b3f15a0911989946f9365f29
hao.lv <1047627965@qq.com>
2024-03-07 16:04:36 +0800

    42

--- src/main/java/com/leetcode/cn/solution1_50/Solution42.java
+++ src/main/java/com/leetcode/cn/solution1_50/Solution42.java
@@ -8,18 +8,6 @@ public class Solution42 {
         int area = 0;
         Stack<Integer> stack = new Stack<>();
         for(int i = 0; i<height.length; i++) {
-            if(stack.isEmpty()) {
-                // 栈为空时加入第一个高度不为0的柱子
-                if(height[i] != 0) {
-                    stack.push(i);
-                }
-                continue;
-            }
-            // 当前柱子比栈顶柱子矮时，当前柱子入栈
-            if(height[i] < height[stack.peek()]) {
-                stack.push(i);
-                continue;
-            }
             // 当前柱子比栈顶柱子高时，栈顶柱子出栈，计算积水容量。
             while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
                 int lastIndex = stack.peek();
@@ -29,14 +17,15 @@ public class Solution42 {
                 }
                 area = area + (Math.min(height[i], height[stack.peek()]) - height[lastIndex]) * (i - stack.peek() - 1);
             }
+            // 栈为空时，当前柱子入栈
             if (stack.isEmpty()) {
                 stack.push(i);
             } else {
-                // while循环结束，当前柱子比栈顶柱子矮
-                if (height[i] != height[stack.peek()]) {
+                // 当前柱子比栈顶柱子矮，当前柱子入栈
+                if (height[i] < height[stack.peek()]) {
                     stack.push(i);
                 }
-                // while循环结束，当前柱子跟栈顶柱子一样高，栈顶柱子出栈，新柱子入栈（为了计算积水容积时能够套用上面的计算公式）
+                // 当前柱子跟栈顶柱子一样高，栈顶柱子出栈，新柱子入栈（为了计算积水容积时能够套用上面的计算公式）
                 else {
                     stack.pop();
                     stack.push(i);
